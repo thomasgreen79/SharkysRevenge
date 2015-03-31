68208f939c4dc7055613349887ae4fc1e5c02d4d
MrGreenIsMe <thomasgreen@u.boisestate.edu>
2015-03-30 21:23:42 -0600

    Mostly functional copy of quiz manager.  Needs string parsing in loadQuiz method, and has non-list code for Quiz Manager for each lesson as separate variable.

--- Testing_Menu/Assets/Scripts/QuizFunctionality.cs
+++ Testing_Menu/Assets/Scripts/QuizFunctionality.cs
@@ -8,19 +8,33 @@ public class QuizFunctionality : MonoBehaviour {
 	public bool selSubmit;
 	public bool showText = false;
 	public bool answeredCorrectly = false;
+	public int quizNum = -1;
+	public int lessonNum = -1;
+	public QuizManagerScript quizManager;
 
 	// Use this for initialization
 	void Start () {
 		selGridInt = -1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
 
-	public string[] selStrings = new string[] {" Answer One", " Answer Two", " Answer Three", " Answer Four"};
+	public string[] selStrings1 = new string[] {
+		" Answer One", 
+		" Answer Two", 
+		" Answer Three", 
+		" Answer Four"};
+
+	public string[] selStrings2 = new string[] {
+		" Scooby", 
+		" Dooby", 
+		" Doo", 
+		" Where Are You"};
+
+	public string[] selStrings3 = new string[] {
+		" Answer One", 
+		" Answer Two", 
+		" Answer Three", 
+		" Answer Four"};
+	
 
 	void OnGUI() {
 
@@ -29,24 +43,31 @@ public class QuizFunctionality : MonoBehaviour {
 		}
 		GUILayout.BeginArea(new Rect((Screen.width/2)-100, (Screen.height/2)-50, 200, 150));
 	
-		selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings, 1, "toggle");
+		selGridInt = GUILayout.SelectionGrid(selGridInt, selStrings2, 1, "toggle");
 		GUILayout.FlexibleSpace();
+		//GUILayout.Button ("Back");
 		selSubmit = GUILayout.Button ("Submit");
 		if (showText) {
 				GUILayout.Label ("Please select an answer first");
 		}
 		if (selSubmit) {
-						if (selGridInt != -1) {
-								Debug.Log ("You chose " + selStrings [selGridInt]);
-								if (selGridInt == answer) {
-									answeredCorrectly = true;
-								}
-								Application.LoadLevel ("Lesson1Quiz1Completed");
-						} else {
-								showText = true;
-								Debug.Log ("You must first select an answer to submit");
-						}
+			if (selGridInt != -1) {
+				Debug.Log ("You chose " + selStrings2 [selGridInt]);
+				Debug.Log("Stored user answer is: " + quizManager.getUserAnswer(lessonNum, quizNum));
+				quizManager.setQuizCompleted(lessonNum, quizNum);
+				quizManager.setUserAnswer(lessonNum, quizNum, selGridInt);
+				if (selGridInt == quizManager.getCorrectAnswer(lessonNum, quizNum)) {
+					answeredCorrectly = true;
 				}
+				Debug.Log("Stored user answer is: " + quizManager.getUserAnswer(lessonNum, quizNum));
+				Debug.Log ("Correct answer is: " + quizManager.getCorrectAnswer(lessonNum, quizNum));
+				Application.LoadLevel ("Lesson" + lessonNum + "Quiz" + quizNum + "Completed");
+			} else {
+				showText = true;
+				Debug.Log ("You must first select an answer to submit");
+			}
+			Debug.Log("quiz number = " + quizNum);
+		}
 		GUILayout.EndArea();
 	}
 }
--- Testing_Menu/Assets/Scripts/QuizManagerScript.cs
+++ Testing_Menu/Assets/Scripts/QuizManagerScript.cs
@@ -4,68 +4,169 @@ using System.Collections.Generic;
 
 public class QuizManagerScript : MonoBehaviour {
 	
-	private static QuizData Lesson1;
-	private static QuizData Lesson2;
-	private static QuizData Lesson3;
-	
-	public void initLesson1(int numQuizzes){
-		List<int> answers = new List<int> ();
-		answers.Add (3);
-		answers.Add (2);
-		answers.Add (1);
-		Lesson1 = new QuizData (numQuizzes, answers);
+//	private static QuizData Lesson1;
+//	private static QuizData Lesson2;
+//	private static QuizData Lesson3;
+	private static List<QuizData> Lessons;
+
+	public void initLessons(){
+		if (Lessons == null) {
+			Lessons = new List<QuizData> ();
+			List<int> answers = new List<int> ();
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			Lessons.Add (new QuizData (answers.Count, answers));
+			List<int> answers1 = new List<int> ();
+			answers1.Add (1);
+			answers1.Add (3);
+			answers1.Add (2);
+			Lessons.Add (new QuizData (answers1.Count, answers1));
+			List<int> answers2 = new List<int> ();
+			answers2.Add (3);
+			answers2.Add (2);
+			answers2.Add (0);
+			Lessons.Add (new QuizData (answers2.Count, answers2));
+		}
+	}
+
+	public int getUserAnswer(int lessonNum, int quizNum){
+		return Lessons[lessonNum-1].getUserAnswer(quizNum-1);
+	}
+
+	public int getCorrectAnswer(int lessonNum, int quizNum){
+		return Lessons[lessonNum-1].getCorrectAnswer(quizNum-1);
+	}
+
+	public void setUserAnswer(int lessonNum, int quizNum, int answer){
+		Lessons[lessonNum-1].setUserAnswer(quizNum-1, answer);
+	}
+
+	public void setQuizCompleted(int lessonNum, int quizNum){
+		Lessons[lessonNum-1].setQuizCompleted (quizNum-1);
+	}
+
+	public void loadQuiz(int lessonNum, int quizNum){
+		if (!Lessons[lessonNum-1].getQuizCompleted(quizNum-1)) {
+			Application.LoadLevel("Lesson" + lessonNum + "Quiz" + quizNum);
+		} else {
+			Application.LoadLevel("Lesson" + lessonNum + "Quiz" + quizNum + "Completed");
+		}
+	}
+
+	public void loadQuizByString(string nums){
+		int lessonNum = 1;
+		int quizNum = 1;
+		if (!Lessons[lessonNum-1].getQuizCompleted(quizNum-1)) {
+			Application.LoadLevel("Lesson" + lessonNum + "Quiz" + quizNum);
+		} else {
+			Application.LoadLevel("Lesson" + lessonNum + "Quiz" + quizNum + "Completed");
+		}
 	}
 	
-	public void initLesson2(int numQuizzes){
-		List<int> answers = new List<int> ();
-		answers.Add (3);
-		answers.Add (2);
-		answers.Add (1);
-		Lesson2 = new QuizData (numQuizzes, answers);
+
+/*
+	public void initLesson1(){
+		if (Lesson1 == null) {
+			List<int> answers = new List<int> ();
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			Lesson1 = new QuizData (answers.Count, answers);
+		}
 	}
 	
-	public void initLesson3(int numQuizzes){
-		List<int> answers = new List<int> ();
-		answers.Add (3);
-		answers.Add (2);
-		answers.Add (1);
-		Lesson3 = new QuizData (numQuizzes, answers);
+	public void initLesson2(){
+		if (Lesson2 == null) {
+			List<int> answers = new List<int> ();
+			answers.Add (3);
+			answers.Add (2);
+			answers.Add (1);
+			Lesson2 = new QuizData (answers.Count, answers);
+		}
 	}
 	
+	public void initLesson3(){
+		if (Lesson3 == null) {
+			List<int> answers = new List<int> ();
+			answers.Add (3);
+			answers.Add (2);
+			answers.Add (1);
+			Lesson3 = new QuizData (answers.Count, answers);
+		}
+	}
+
+	public int getUserAnswerInOne(int quizNum){
+		return Lesson1.getUserAnswer(quizNum-1);
+	}
+
+	public int getUserAnswerInTwo(int quizNum){
+		return Lesson2.getUserAnswer(quizNum-1);
+	}
+	
+	public int getUserAnswerInThree(int quizNum){
+		return Lesson3.getUserAnswer(quizNum-1);
+	}
+
+	public int getCorrectAnswerInOne(int quizNum){
+		return Lesson1.getCorrectAnswer(quizNum-1);
+	}
+
+	public int getCorrectAnswerInTwo(int quizNum){
+		return Lesson2.getCorrectAnswer(quizNum-1);
+	}
+
+	public int getCorrectAnswerInThree(int quizNum){
+		return Lesson3.getCorrectAnswer(quizNum-1);
+	}
+	
+	public void setUserAnswerInOne(int quizNum, int answer){
+		Lesson1.setUserAnswer(quizNum-1, answer);
+	}
+
+	public void setUserAnswerInTwo(int quizNum, int answer){
+		Lesson2.setUserAnswer(quizNum-1, answer);
+	}
+
+	public void setUserAnswerInThree(int quizNum, int answer){
+		Lesson3.setUserAnswer(quizNum-1, answer);
+	}
+
 	public void setQuizCompletedInOne(int quizNum){
-		Lesson1.setQuizCompleted (quizNum);
+		Lesson1.setQuizCompleted (quizNum-1);
 	}
 	
 	public void setQuizCompletedInTwo(int quizNum){
-		Lesson2.setQuizCompleted (quizNum);
+		Lesson2.setQuizCompleted (quizNum-1);
 	}
 	
 	public void setQuizCompletedInThree(int quizNum){
-		Lesson3.setQuizCompleted (quizNum);
+		Lesson3.setQuizCompleted (quizNum-1);
 	}
 	
 	public void loadQuizInOne(int quizNum){
-		if (!Lesson1.getQuizCompleted(quizNum)) {
+		if (!Lesson1.getQuizCompleted(quizNum-1)) {
 			Application.LoadLevel("Lesson1Quiz" + quizNum);
 		} else {
-			Application.LoadLevel("Lesson1QuizCompleted" + quizNum);
+			Application.LoadLevel("Lesson1Quiz" + quizNum + "Completed");
 		}
 	}
 	
 	public void loadQuizInTwo(int quizNum){
-		if (!Lesson2.getQuizCompleted(quizNum)) {
+		if (!Lesson2.getQuizCompleted(quizNum-1)) {
 			Application.LoadLevel("Lesson2Quiz" + quizNum);
 		} else {
-			Application.LoadLevel("Lesson2QuizCompleted" + quizNum);
+			Application.LoadLevel("Lesson2Quiz" + quizNum + "Completed");
 		}
 	}
 	
 	public void loadQuizInThree(int quizNum){
-		if (!Lesson3.getQuizCompleted(quizNum)) {
+		if (!Lesson3.getQuizCompleted(quizNum-1)) {
 			Application.LoadLevel("Lesson3Quiz" + quizNum);
 		} else {
-			Application.LoadLevel("Lesson3QuizCompleted" + quizNum);
+			Application.LoadLevel("Lesson3Quiz" + quizNum + "Completed");
 		}
 	}
+	*/
 }
 
