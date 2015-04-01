dc7a9e888d5f6a15306fcdc0b8f3c640a4315ec9
MrGreenIsMe <thomasgreen@u.boisestate.edu>
2015-04-01 15:38:32 -0600

    Updating current versions of quiz scripts and QuizData unit tests.  Also, updated versions of Quiz1-1 and completed quiz and changed a button in Lesson1Page5

--- Testing_Menu/Assets/Scripts/QuizManagerScript.cs
+++ Testing_Menu/Assets/Scripts/QuizManagerScript.cs
@@ -6,45 +6,98 @@ namespace Quizzes{
 public class QuizManagerScript : MonoBehaviour {
 	
 	private static List<QuizData> Lessons;
+	private string[] selStrings1 = new string[] {
+		" Answer One", 
+		" Answer Two", 
+		" Answer Three", 
+		" Answer Four"};
+		
+	private string[] selStrings2 = new string[] {
+		" Scooby", 
+		" Dooby", 
+		" Doo", 
+		" Where Are You"};
+	
+	private string[] selStrings3 = new string[] {
+		" Answer One", 
+		" Answer Two", 
+		" Answer Three", 
+		" Answer Four"};
 
 	public void initLessons(){
 		if (Lessons == null) {
 			Lessons = new List<QuizData> ();
+			List<string[]> texts = new List<string[]>();
+			texts.Add (selStrings1);
+			texts.Add (selStrings2);
+			texts.Add (selStrings3);
+			
 			List<int> answers = new List<int> ();
 			answers.Add (2);
 			answers.Add (0);
 			answers.Add (1);
-			Lessons.Add (new QuizData (answers.Count, answers));
+			Lessons.Add (new QuizData (answers.Count, answers, texts));
+
 			List<int> answers1 = new List<int> ();
 			answers1.Add (1);
 			answers1.Add (3);
 			answers1.Add (2);
-			Lessons.Add (new QuizData (answers1.Count, answers1));
+			Lessons.Add (new QuizData (answers1.Count, answers1, texts));
+
 			List<int> answers2 = new List<int> ();
 			answers2.Add (3);
 			answers2.Add (2);
 			answers2.Add (0);
-			Lessons.Add (new QuizData (answers2.Count, answers2));
+			Lessons.Add (new QuizData (answers2.Count, answers2, texts));
 		}
 	}
 
 	public int getUserAnswer(int lessonNum, int quizNum){
+		if (Lessons == null) {
+			initLessons ();
+		}
 		return Lessons[lessonNum-1].getUserAnswer(quizNum-1);
 	}
 
 	public int getCorrectAnswer(int lessonNum, int quizNum){
+		if (Lessons == null) {
+			initLessons ();
+		}
 		return Lessons[lessonNum-1].getCorrectAnswer(quizNum-1);
 	}
 
+	public int getNumPossibleAnswers(int lessonNum, int quizNum){
+		if (Lessons == null) {
+			initLessons ();
+		}
+		return Lessons[lessonNum-1].getNumPossibleAnswers (quizNum-1);
+	}
+
+	public string[] getQuizTexts(int lessonNum, int quizNum){
+		if (Lessons == null) {
+			initLessons ();
+		}
+		return Lessons [lessonNum - 1].getTexts (quizNum);
+	}
+
 	public void setUserAnswer(int lessonNum, int quizNum, int answer){
+		if (Lessons == null) {
+			initLessons ();
+		}
 		Lessons[lessonNum-1].setUserAnswer(quizNum-1, answer);
 	}
 
 	public void setQuizCompleted(int lessonNum, int quizNum){
+		if (Lessons == null) {
+			initLessons ();
+		}
 		Lessons[lessonNum-1].setQuizCompleted (quizNum-1);
 	}
 
 	public void loadQuiz(int lessonNum, int quizNum){
+		if (Lessons == null) {
+			initLessons ();
+		}
 		if (!Lessons[lessonNum-1].getQuizCompleted(quizNum-1)) {
 			Application.LoadLevel("Lesson" + lessonNum + "Quiz" + quizNum);
 		} else {
@@ -53,6 +106,9 @@ public class QuizManagerScript : MonoBehaviour {
 	}
 
 	public void loadQuizByString(string nums){
+		if (Lessons == null) {
+			initLessons ();
+		}
 		int lessonNum = 1;
 		int quizNum = 1;
 		if (!Lessons[lessonNum-1].getQuizCompleted(quizNum-1)) {
--- Testing_Menu/Assets/UnityTestTools/UnitTesting/Editor/QuizTest.cs
+++ Testing_Menu/Assets/UnityTestTools/UnitTesting/Editor/QuizTest.cs
@@ -7,15 +7,96 @@ namespace Quizzes{
 	[TestFixture]
 	public class QuizTests{
 		[Test]
-		public void getCorrectAnswer(){
+		public void getQuizNotCompleted(){
 			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
 			answers.Add (2);
 			answers.Add (0);
 			answers.Add (1);
-			QuizData qd = new QuizData (3, answers);
+			QuizData qd = new QuizData (3, answers, texts);
+
+			bool result = qd.getQuizCompleted (0);
+			Assert.AreEqual (false, result);
+		}
+		[Test]
+		public void getQuizCompleted(){
+			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			QuizData qd = new QuizData (3, answers, texts);
+
+			qd.setQuizCompleted (0);
+			bool result = qd.getQuizCompleted (0);
+			Assert.AreEqual (true, result);
+		}
+		[Test]
+		public void getCorrectAnswer(){
+			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			QuizData qd = new QuizData (3, answers, texts);
 			
 			int result = qd.getCorrectAnswer (0);
 			Assert.AreEqual (2, result);
+		}
+		[Test]
+		public void getUserNoAnswer(){
+			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			QuizData qd = new QuizData (3, answers, texts);
+
+			int result = qd.getUserAnswer (0);
+			Assert.AreEqual (-1, result);
+		}
+		[Test]
+		public void getUserAnswer(){
+			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			QuizData qd = new QuizData (3, answers, texts);
+
+			qd.setUserAnswer (0, 3);
+			int result = qd.getUserAnswer (0);
+			Assert.AreEqual (3, result);
+		}
+		[Test]
+		public void getQuizTexts(){
+			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
+			string[] testTexts = new string[1];
+			testTexts [0] = "testText";
+			texts.Add (testTexts);
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			QuizData qd = new QuizData (3, answers, texts);
+
+			string result = qd.getTexts (0) [0];
+			Assert.AreEqual ("testText", result);
+		}
+		[Test]
+		public void getNumAnswers(){
+			List<int> answers = new List<int> ();
+			List<string[]> texts = new List<string[]> ();
+			string[] testTexts = new string[1];
+			testTexts [0] = "testText";
+			texts.Add (testTexts);
+			answers.Add (2);
+			answers.Add (0);
+			answers.Add (1);
+			QuizData qd = new QuizData (3, answers, texts);
+			
+			int result = qd.getNumPossibleAnswers(0);
+			Assert.AreEqual (1, result);
 		}
 	}
 }
